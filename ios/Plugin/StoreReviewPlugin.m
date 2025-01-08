f2eefff248aad5baaf79cc4bc228cdffcce2873c
Stepan Kotov <rediska1114@gmail.com>
2025-01-08 22:30:23 +0500

    fix: fix ios methods

--- ios/Plugin/StoreReviewPlugin.m
+++ ios/Plugin/StoreReviewPlugin.m
@@ -5,5 +5,5 @@
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(StoreReviewPlugin, "StoreReview",
            CAP_PLUGIN_METHOD(navigateToAppStore, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(promtForRating, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(requestReview, CAPPluginReturnPromise);
 )
